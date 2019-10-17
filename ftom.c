c02cf08c07251f62db58e0288bf7f5b94ef1a0fb
lysdexic-audio <43569216+lysdexic-audio@users.noreply.github.com>
2019-10-17 13:13:28 +1100

    Update ftom.c

--- ftom.c
+++ ftom.c
@@ -1,4 +1,18 @@
-/* listing 1.2. Calculate MIDI Note number from frequency */
+/* listing 1.2. Calculate MIDI Note number from frequency
+
+We define pitchbend as a percentage deviation half a semitone above or below a MIDI note.
+Maximum deviation is therefore +-50 percent.
+For example, given a frequency of 430 Hz, the nearest notes are 69 (f = 440) and 68 (f = 415.3).
+
+The deviation is therefore: 10/24.7 = -0.398 = -40 percent (approximately).
+
+Extend freq2midi to report the frequency deviation above or below the calculated MIDI note, as a percentage.
+
+(Hint: To print a per- centage symbol using printf, precede it with the same symbol.)
+         int bend = 45;
+         printf("pitchbend = %d%%\n",bend);
+*/
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -7,13 +21,15 @@ int main(int argc, char *argv[])
 {
 
   double freqArg = atof (argv[1]);
-  printf("%f\n", freqArg);
   double fracmidi;
   double semitone_ratio;
   double c0; /* for frequency of MIDI Note 0 */
   double c5; /* for frequency of Middle C */
   double frequency; /* ... which we want to find */
   int midinote; /* given this note */
+  int midilow;
+  double pitchbend;
+  double integral;
 
   semitone_ratio = pow(2, 1/12.0); /* approx 1.0594631 */
   /* find middle C, three semitones above low A = 220 */
@@ -28,9 +44,15 @@ int main(int argc, char *argv[])
   */
   frequency = freqArg;
   fracmidi = log(frequency / c0) / log(semitone_ratio);
-  /* round fracmidi to the nearest whole number */
+  /* round fracmidi up to the nearest whole number
   midinote = (int) (fracmidi + 0.5);
-  printf("The nearest MIDI note to the frequency %f is %d\n", frequency, midinote);
+   */
 
+  pitchbend = modf(fracmidi, &integral) * 100;
+  midinote = integral;
+  
+  printf("The nearest MIDI note to the frequency %f is %d\n", frequency, midinote);
+  printf("pitchbend = %f%%\n", pitchbend);
+  printf("Raw fracmidi = %f\n", fracmidi);
   return 0;
 }
